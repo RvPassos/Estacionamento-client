804533bcece192c95700f464a9e6a5108aafc730
roberto <rvpassos@hotmail.com>
2023-06-15 01:01:27 -0300

    configurando rota e importando bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/marca">Marca</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style lang="scss">
+
+
+
+@import "~/node_modules/bootstrap/scss/bootstrap";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
